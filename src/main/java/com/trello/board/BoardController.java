849cd6e77a26de0b3c40f45bb8c414908924a0ea
frs <frs2000@mail.ru>
2019-11-24 21:38:11 +0200

    Rewrote all methods with ResponseEntity (class BoardController).

--- src/main/java/com/trello/board/BoardController.java
+++ src/main/java/com/trello/board/BoardController.java
@@ -2,6 +2,8 @@ package com.trello.board;
 
 import com.trello.column.dto.ColumnBoardDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +20,7 @@ public class BoardController {
     }
 
     @GetMapping("/getByID")
-    public ColumnBoardDTO getByID(@RequestParam int id) {
-        return boardService.getByID(id);
+    public ResponseEntity<ColumnBoardDTO> getByID(@RequestParam int id) {
+        return new ResponseEntity<>(boardService.getByID(id), HttpStatus.OK);
     }
 }
